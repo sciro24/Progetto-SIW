9f00df43ba00ca049ccd8ee74dde6aabd304da01
Diego Scirocco <diego.scirocco@gmail.com>
2024-09-04 13:19:02 +0200

    4.5 fix import.sql

--- siw-ricette/src/main/resources/import.sql
+++ siw-ricette/src/main/resources/import.sql
@@ -1,7 +1,7 @@
 -- Inserimento Utente
 insert into utente(id, cognome, email, nome) values (nextval('utente_seq'), 'Scirocco', 'diego.scirocco@gmail.com', 'Diego');
 insert into utente(id, cognome, email, nome) values (nextval('utente_seq'), 'Marconi','marco.marconi@gmail.com' , 'Marco');
-insert into utente(id, cognome, email, nome) values (nextval('utente_seq'), 'Macale', 'andriu.macale@hotmail.com', 'Andrea');
+insert into utente(id, cognome, email, nome) values (nextval('utente_seq'), 'Andreoni', 'andriu@hotmail.com', 'Andrea');
 insert into utente(id, cognome, email, nome) values (nextval('utente_seq'), 'Scirocco', 'lorenzo.scirocco@gmail.com', 'Lorenzo');
 insert into utente(id, cognome, email, nome) values (nextval('utente_seq'), 'Bianchi', 'bianchi@gmail.com', 'Fede');
 insert into utente(id, cognome, email, nome) values (nextval('utente_seq'), 'Neri', 'neri@gmail.com', 'Fra');
@@ -10,7 +10,7 @@ insert into utente(id, cognome, email, nome) values (nextval('utente_seq'), 'Ner
 -- Inserimento Credenziali
 insert into credenziali(id, password, role, username, utente_id) values (nextval('credenziali_seq'), '$2a$10$p6sFaE0TOXvkmrOUXjdFXuiCq7HkALiEwi1KVhhmqyYZvefF3lE7K', 'ADMIN', 'Diego', (select id from utente where email = 'diego.scirocco@gmail.com'));
 insert into credenziali(id, password, role, username, utente_id) values (nextval('credenziali_seq'), '$2a$10$36fHPrcZ5kjTLgxtbB8FdeL5ETeDyCBDSvIKbI7n6AJbqs/c4flmS', 'DEFAULT', 'Marco', (select id from utente where email = 'marco.marconi@gmail.com'));
-insert into credenziali(id, password, role, username, utente_id) values (nextval('credenziali_seq'), '$2a$10$S6HueP.58CzXZhQtat/sGOHgpoRSDpYQ835AA3Gq1Pu2dOijVLzCS', 'DEFAULT', 'Andrea', (select id from utente where email = 'andriu.macale@hotmail.com')); 
+insert into credenziali(id, password, role, username, utente_id) values (nextval('credenziali_seq'), '$2a$10$S6HueP.58CzXZhQtat/sGOHgpoRSDpYQ835AA3Gq1Pu2dOijVLzCS', 'DEFAULT', 'Andrea', (select id from utente where email = 'andriu@hotmail.com')); 
 insert into credenziali(id, password, role, username, utente_id) values (nextval('credenziali_seq'), '$2a$10$KTHFybov.hr94njClsS7qu/V21ngPGM9nCJlBbi6cOAOP7urmTQMe', 'ADMIN', 'Lorenzo', (select id from utente where email = 'lorenzo.scirocco@gmail.com'));
 insert into credenziali(id, password, role, username, utente_id) values (nextval('credenziali_seq'), '$2a$10$qE/4gbq.UiOrY8shvwk/VOO/3d3KJ6F7qmmyhVKJjZGjgk2mV5SFm', 'DEFAULT', 'Fede', (select id from utente where email = 'bianchi@gmail.com'));
 insert into credenziali(id, password, role, username, utente_id) values (nextval('credenziali_seq'), '$2a$10$vInxSibcoAYUgjt6BFjluOLRCqL8/QJdwLpHQc6LA3xwQq8lprpCm', 'DEFAULT', 'Fra', (select id from utente where email = 'neri@gmail.com')); 
